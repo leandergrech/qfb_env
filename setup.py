cde8cc8bfcfb855deed131b66cf9e5a33fa5eb7f
Leander Grech <grechleander@gmail.com>
2022-04-27 01:40:05 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+	name='qfb_env',
+	version='1.0',
+	description='QFBEnv Reinforcement Learning OpenAI Gym environment.',
+	author='lgrech',
+	install_requires=['gym', 'scipy', 'sklearn']
+)
